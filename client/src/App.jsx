0a4b9039b818a3baf893bb85ba90b86ba13a4ffe
Yoki <thekingmaker0012@gmail.com>
2024-09-25 13:40:30 +0530

    Front-end changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,35 @@
 import { useEffect } from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 export const API_URL = "http://localhost:3001/api/users";
 
 const App = () => {
   useEffect(() => {
     const fetchData = async () => {
-      const token = localStorage.getItem("token");
-      console.log("token", token);
-
-      // await fetch(API_URL).then((res) => console.log("data", res));
+      console.log("fetch data");
     };
+
     fetchData();
   }, []);
 
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="*" element={<p>Page not Found!</p>} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
