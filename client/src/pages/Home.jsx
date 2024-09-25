00c78b4c23a75d20be7f8872a3fdfb4d723e91f6
Yoki <thekingmaker0012@gmail.com>
2024-09-25 13:40:48 +0530

    Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,15 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>
+        <h2>Home</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium,
+          velit.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
