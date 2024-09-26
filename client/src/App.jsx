4ea52f8b2f659ca023e90085fcee6d1e98bf621d
Yoki <thekingmaker0012@gmail.com>
2024-09-26 20:56:52 +0530

    Updated header with profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,29 @@
-import { useEffect } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 export const API_URL = "http://localhost:3001/api";
 
 const App = () => {
-  useEffect(() => {
-    // const fetchData = async () => {
-    //   console.log("fetch data");
-    // };
-    // fetchData();
-  }, []);
-
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+
         <Route path="*" element={<p>Page not Found!</p>} />
       </Routes>
     </>
