c239e4f125e8e0a8e94bc82b5ae88dba6e32f19f
Yoki <thekingmaker0012@gmail.com>
2024-09-27 20:50:02 +0530

    App done!

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h2>About Page</h2>
+
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero cumque
+        sapiente dolore asperiores sit magnam illum repellendus tempore natus
+        porro.
+      </p>
+    </div>
+  );
 };
 
 export default About;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,23 @@
+import { useSelector } from "react-redux";
+
 const Home = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
+  console.log("currentUser", currentUser);
+
   return (
     <>
       <div>
-        <p>HOMEPAGE</p>
+        <h1>Welcome to my Auth App, {currentUser?.username.toUpperCase()}</h1>
+        <br />
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur,
+          officiis molestias. Explicabo, voluptas impedit exercitationem quo
+          itaque harum deleniti. Consequatur eum velit ad ipsam temporibus
+          perferendis cupiditate natus dolorem est! Explicabo, obcaecati
+          doloremque. Itaque quis rem corrupti illo incidunt, mollitia ipsam
+          modi eligendi quaerat, ut, optio placeat dolorem enim ipsa.
+        </p>
       </div>
     </>
   );
