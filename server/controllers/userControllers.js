30ada5deadf89bd313676cea8a5b28c36da8f43d
Yoki <thekingmaker0012@gmail.com>
2024-09-27 17:40:28 +0530

    added updateuser in server

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,11 +1,43 @@
+const errorHandler = require("../utilities/error");
+const bcrypt = require("bcryptjs");
+const User = require("../model/userModel");
+
 const test = (req, res) => {
   res.json({ msg: "test is working!", name: "rocky" });
   console.log("test is working!");
 };
 
-//update user
+//update users
 const updateUser = async (req, res, next) => {
-  console.log("update user is working!");
+  if (req.user.id !== req.params.id) {
+    next(errorHandler(401, "You can update only your account!"));
+  }
+
+  try {
+    if (req.body.password) {
+      req.body.password = bcrypt.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          profilePicture: req.body.profilePicture,
+        },
+      },
+      { new: true } //displays the updated user
+    );
+
+    const { password: hased, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = { test, updateUser };
+
+//http://localhost:3001/api/user/update/66f50959802e9e83a059af1f
